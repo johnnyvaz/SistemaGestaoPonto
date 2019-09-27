28120825601a91ecf6f21bbccb33df6bbb26d5cd
johnnyvaz1 <johnnyjohnnyjohnnyvaz@gmail.com>
2019-09-27 07:30:12 -0300

    add docker

--- public/index.php
+++ public/index.php
@@ -9,7 +9,10 @@ $user = new User(['name'=> 'lucas', 'email' => 'lucas@teste.com']);
 
 echo User::getSelect(['id' => 1, 'name , email']);
 
-
+foreach(User::get([], 'name') as $user){
+    echo $user->name;
+    echo '<br>'
+}
 
 // $sql = 'select * from users';
 // $result = Database::getResultFromQuery($sql);
