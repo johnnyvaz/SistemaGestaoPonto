8fcbe4378bad360c371c8d488e5f9720f73d6906
johnnyvaz1 <johnnyjohnnyjohnnyvaz@gmail.com>
2019-10-01 13:28:15 -0300

    docker funcionando somente php

--- public/index.php
+++ public/index.php
@@ -1,18 +1,8 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
 
-$user = new User(['name'=> 'lucas', 'email' => 'lucas@teste.com']);
-// $user->set('email','lucas_alterado@coder.com');
-// print_r($user->get('email'));
-
-echo User::getSelect(['id' => 1, 'name , email']);
-
-foreach(User::get([], 'name') as $user){
-    echo $user->name;
-    echo '<br>'
-}
 
 // $sql = 'select * from users';
 // $result = Database::getResultFromQuery($sql);
---
+++ src/views/login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>In 'n Out</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="assets/css/comum.css" rel="stylesheet">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/css/icofont.min.css" rel="stylesheet">
+        <link href="assets/css/login.css" rel="stylesheet">
+    </head>
+    <body>
+    <form class="form-login" action="#" method="post">
+        <div class="login-card card">
+            <div class="card-header">
+                <i class="icofont-travelling mr-2"></i>
+                <span class="font-weight-light">In </span>
+                <span class="font-weight-bold mx-2">N' </span>
+                <span class="font-weight-light">Out</span>
+                <i class="icofont-runner-alt-1 ml-2"></i>
+            </div>
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="email" >E-mail</label>
+                    <input type="email" name="email" id="email"
+                        class="form-control" 
+                        placeholder="Infome o e-mail" autofocus>
+                </div>
+                <div class="form-group">
+                    <label for="password" >Senha</label>
+                    <input type="password" name="password" id="password"
+                        class="form-control" 
+                        placeholder="Infome a senha" >
+                </div>
+                </div>
+                <div class="card-footer">
+                <button class="btn btn-primary">Entrar</button>
+                </div>
+            
+        </div>
+
+    </form>
+    </body>
+</html>
